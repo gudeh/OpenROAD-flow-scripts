998c22b203b45923ee144b0a721de2e78d168337
gudeh <gudeh@gmail.com>
2022-07-16 17:05:25 -0300

    july update

--- flow/myRunAll.sh
+++ flow/myRunAll.sh
@@ -6,8 +6,9 @@ while read line; do echo "-->myRunall.sh: cleaning $line" && make clean_all DESI
 
 while read line; do echo "-->myRunall.sh: loading $line" && make DESIGN_CONFIG=$line && make getFeatures DESIGN_CONFIG=$line && make my_gui DESIGN_CONFIG=$line; done < myStuff/designsToRun.txt #&& python3 myStuff/gateToHeat.py $DESIGN_NAME > myStuff/Python.out; done < myStuff/designsToRun.txt
 
-printf ">>>>Done with OpenROAD runs!\n>>>Executing Python to handle all CSVs!\n"
+printf ">>>>Done with OpenROAD runs!\n>>>>Handling all CSVs!\n"
 #printf "design config $DESIGN_CONFIG\n"
 #printf "platform $PLATFORM\n"
-python3 myStuff/gateToHeat.py > myStuff/Python.out
+#python3 myStuff/gateToHeat.py > myStuff/Python.out
+./gatetoheat > myStuff/gateToHeatCPP.out
 printf "myRunall.sh: finished!\n"
