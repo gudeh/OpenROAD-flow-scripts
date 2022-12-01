02168f3c82276da5c783fb451e228d5afbf62429
gudeh <gudeh@gmail.com>
2022-12-01 14:10:06 -0300

    minor update with prints

--- flow/scripts/getFeatures.tcl
+++ flow/scripts/getFeatures.tcl
@@ -1,4 +1,4 @@
-puts "Running getFeatures.tcl"
+puts "\nRunning getFeatures.tcl"
 puts "::env(RESULTS_DIR)-> $::env(RESULTS_DIR)"
 
 yosys -import
@@ -6,4 +6,5 @@ read_liberty -lib {*}$::env(DONT_USE_LIBS)
 read_verilog $::env(RESULTS_DIR)/1_1_yosys.v
 hierarchy -auto-top
 hello_world
+puts "\nExit getFeatures.tcl"
 exit
