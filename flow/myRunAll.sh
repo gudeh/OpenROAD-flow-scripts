f6ca713b52ed6317513f16d9e9792e2ce5259951
gudeh <gudehh@gmail.com>
2023-03-31 11:31:55 -0300

    fix gate name yosys versus OR, reuse OR run on myRunAll

--- flow/myRunAll.sh
+++ flow/myRunAll.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-while read line; do 
-  echo "-->myRunall.sh: cleaning $line" #&& 
-  make clean_all DESIGN_CONFIG=$line; 
-done < congestionPrediction/designsToRun.txt
+#while read line; do 
+#  echo "-->myRunall.sh: cleaning $line" #&& 
+#  make clean_all DESIGN_CONFIG=$line; 
+#done < congestionPrediction/designsToRun.txt
 
 #make clean_all
 
+set doClean false
+set doRun false
+
 while read line; do 
-  echo "-->myRunall.sh: loading $line" #&&
-  make DESIGN_CONFIG=$line #&& 
-  make getFeatures DESIGN_CONFIG=$line #&& 
-  make my_gui DESIGN_CONFIG=$line; 
-#  make gui_final DESIGN_CONFIG=$line; 
+
+    if [ "$doClean" = true ] ; then
+        echo "-->myRunall.sh: cleaning $line" #&& 
+        make clean_all DESIGN_CONFIG=$line; 
+    fi
+  
+    if [ "$doRun" = true ] ; then
+        echo "-->myRunall.sh: loading $line" #&&
+        make DESIGN_CONFIG=$line #&& 
+    fi
+    
+    make getFeatures DESIGN_CONFIG=$line #&& 
+    make my_gui DESIGN_CONFIG=$line; 
+    #  make gui_final DESIGN_CONFIG=$line; 
+    
 done < congestionPrediction/designsToRun.txt 
 
 printf "\n\n>>>>Done with OpenROAD runs!\n\n"
