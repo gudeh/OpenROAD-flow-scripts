308e6c59dab87204af94f04ee38d39ee968dc247
gudeh <gudehh@gmail.com>
2023-06-12 17:18:53 -0300

    add clean up

--- flow/myRunAll.sh
+++ flow/myRunAll.sh
@@ -3,20 +3,27 @@
 doClean=true
 doRun=true
 
+if [ "$doClean" = true ] ; then
+    echo "--> rm -rf congestionPrediction/dataSet/*"
+    rm -rf congestionPrediction/dataSet/*
+    echo "--> rm -rf congestionPrediction/layoutPrints/*"
+    rm -rf congestionPrediction/layoutPrints/*
+fi
+
 while read line; do 
 
     if [ "$doClean" = true ] ; then
-        echo "-->myRunall.sh: cleaning $line" #&& 
-        make clean_all DESIGN_CONFIG=$line; 
+        echo "--> myRunall.sh: cleaning $line" #&& 
+        make clean_all DESIGN_CONFIG=$line;
     fi
   
     if [ "$doRun" = true ] ; then
-        echo "-->myRunall.sh: loading $line" #&&
-        make DESIGN_CONFIG=$line #&& 
+        echo "--> myRunall.sh: loading $line" #&&
+        make DESIGN_CONFIG=$line PLACE_DENSITY=0.85 PLACE_DENSITY_LB_ADDON= #&& 
     fi
     
     make getFeatures DESIGN_CONFIG=$line #&& 
-    make my_gui DESIGN_CONFIG=$line; 
+    make my_gui DESIGN_CONFIG=$line
     #  make gui_final DESIGN_CONFIG=$line; 
     
 done < congestionPrediction/designsToRun.txt 
@@ -29,8 +36,6 @@ printf "\n\n>>>>Done with OpenROAD runs!\n\n"
 command="./gateToHeat > congestionPrediction/gateToHeatCPP.log"
 printf "\n######\n$command\n######\n\n"
 eval $command
-
-
 
 # performing training 
 #command="cd congestionPrediction && python3 regression.py > regression.log"
