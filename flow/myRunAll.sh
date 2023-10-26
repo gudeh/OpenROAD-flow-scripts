4aa5677ed9909de3308e3531ad4f91238ca77b53
Augusto Berndt <37420320+gudeh@users.noreply.github.com>
2023-10-26 11:30:49 -0300

    Update myRunAll.sh

--- flow/myRunAll.sh
+++ flow/myRunAll.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-doClean=true
+doMyCleanAll=true
+doORCleanIter=true
 doRun=true
 
-if [ "$doClean" = true ] ; then
+#TODO fix rm of dataSet
+if [ "$doMyClean" = true ] ; then
     echo "--> rm -rf congestionPrediction/dataSet/*"
     rm -rf congestionPrediction/dataSet/*
     echo "--> rm -rf congestionPrediction/layoutPrints/*"
     rm -rf congestionPrediction/layoutPrints/*
 fi
 
-while read line; do 
 
-    if [ "$doClean" = true ] ; then
+while read line; do 
+   
+    if [ "$doORCleanIter" = true ] ; then
         echo "--> myRunall.sh: cleaning $line" #&& 
         make clean_all DESIGN_CONFIG=$line;
     fi
   
     if [ "$doRun" = true ] ; then
         echo "--> myRunall.sh: loading $line" #&&
-        make DESIGN_CONFIG=$line PLACE_DENSITY=0.85 PLACE_DENSITY_LB_ADDON= #&& 
+        make DESIGN_CONFIG=$line #PLACE_DENSITY=0.95 #PLACE_DENSITY_LB_ADDON= #&& 
     fi
-    
+
+    #TODO rename my_gui to getLabel
     make getFeatures DESIGN_CONFIG=$line #&& 
     make my_gui DESIGN_CONFIG=$line
     #  make gui_final DESIGN_CONFIG=$line; 
@@ -37,10 +41,13 @@ command="./gateToHeat > congestionPrediction/gateToHeatCPP.log"
 printf "\n######\n$command\n######\n\n"
 eval $command
 
+command="python3 getBasicFeatures.py > congestionPrediction/getBasicFeatures.log"
+printf "\n######\n$command\n######\n\n"
+eval $command
+
 # performing training 
 #command="cd congestionPrediction && python3 regression.py > regression.log"
 #printf "\n######\n$command\n######\n\n"
 #eval $command
-
 
 printf "\nmyRunall.sh: finished!\n"
