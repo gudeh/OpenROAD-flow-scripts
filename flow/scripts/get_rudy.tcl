43d5062386fedb1f0fb6d05c2167a1e9a0da65e8
gudeh <gudehh@gmail.com>
2023-12-13 19:10:07 -0300

    Testing RUDY behaviour versus standard global routing
    Removing derating for congestion map geration
    
    Signed-off-by: Augusto Berndt <gudehh@gmail.com>

--- flow/scripts/get_rudy.tcl
+++ flow/scripts/get_rudy.tcl
@@ -2,10 +2,10 @@ source scripts/gui.tcl
 gui::dump_heatmap Routing "$::env(REPORTS_DIR)/$::env(DESIGN_NAME)-$::env(PLATFORM)-grt.csv"
 gui::set_display_controls "Heat Maps/Routing Congestion" visible true
 set psName $::env(REPORTS_DIR)/$::env(DESIGN_NAME)-$::env(PLATFORM)-grt.png
-save_image $psName
+save_image -width 3000 $psName
 
 gui::dump_heatmap RUDY    "$::env(REPORTS_DIR)/$::env(DESIGN_NAME)-$::env(PLATFORM)-rudy.csv"
 gui::set_display_controls "Heat Maps/Estimated Congestion (RUDY)" visible true
 set psName $::env(REPORTS_DIR)/$::env(DESIGN_NAME)-$::env(PLATFORM)-rudy.png
-save_image $psName
+save_image -width 3000 $psName
 exit
