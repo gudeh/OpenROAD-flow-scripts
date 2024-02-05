035ae96ca91f48c1774086ffd84b1e5e9897adca
Augusto Berndt <gudehh@gmail.com>
2024-02-05 17:31:16 -0300

    add command to save DRCs
    Signed-off-by: Augusto Berndt <gudehh@gmail.com>

--- flow/scripts/get_rudy.tcl
+++ flow/scripts/get_rudy.tcl
@@ -1,5 +1,10 @@
 source scripts/my_gui.tcl
 # $design_stage defined at gui.tcl
+
+gui::load_drc $::env(REPORTS_DIR)/5_route_drc.rpt-5.rpt
+set psName $::env(REPORTS_DIR)/$::env(DESIGN_NAME)-$::env(PLATFORM)-stg$design_stage-DRC.png
+save_image -width 3000 $psName
+
 gui::dump_heatmap Routing "$::env(REPORTS_DIR)/$::env(DESIGN_NAME)-$::env(PLATFORM)-stg$design_stage-grt.csv"
 gui::set_display_controls "Heat Maps/Routing Congestion" visible true
 set psName $::env(REPORTS_DIR)/$::env(DESIGN_NAME)-$::env(PLATFORM)-stg$design_stage-grt.png
@@ -9,4 +14,6 @@ gui::dump_heatmap RUDY    "$::env(REPORTS_DIR)/$::env(DESIGN_NAME)-$::env(PLATFO
 gui::set_display_controls "Heat Maps/Estimated Congestion (RUDY)" visible true
 set psName $::env(REPORTS_DIR)/$::env(DESIGN_NAME)-$::env(PLATFORM)-stg$design_stage-rudy.png
 save_image -width 3000 $psName
+
+unset sdc_file s design_stage
 exit
