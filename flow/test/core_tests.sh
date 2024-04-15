63348d608362fec081e45a4b07192c80d9008247
Augusto Berndt <gudehh@gmail.com>
2024-04-15 17:54:29 -0300

    incldue sky130hd and more waits
    Signed-off-by: Augusto Berndt <gudehh@gmail.com>

--- flow/test/core_tests.sh
+++ flow/test/core_tests.sh
@@ -1,6 +1,7 @@
 test/test_helper.sh gcd nangate45 &
 test/test_helper.sh aes nangate45 &
 test/test_helper.sh tinyRocket nangate45 &
+wait
 test/test_helper.sh dynamic_node nangate45 &
 test/test_helper.sh jpeg nangate45 &
 test/test_helper.sh ibex nangate45 &
@@ -9,14 +10,25 @@ wait
 test/test_helper.sh gcd sky130hs &
 test/test_helper.sh ibex sky130hs &
 test/test_helper.sh jpeg sky130hs &
+wait
 test/test_helper.sh riscv32i sky130hs &
 test/test_helper.sh coyote_tc sky130hs &
 test/test_helper.sh aes sky130hs &
 wait
 
+test/test_helper.sh gcd sky130hd &
+test/test_helper.sh ibex sky130hd &
+test/test_helper.sh jpeg sky130hd &
+wait
+test/test_helper.sh riscv32i sky130hd &
+test/test_helper.sh coyote_tc sky130hd &
+test/test_helper.sh aes sky130hd &
+wait
+
 test/test_helper .sh aes gf180 &
 test/test_helper.sh aes-hybrid gf180 &
 test/test_helper.sh ibex gf180 &
+wait
 test/test_helper.sh jpeg gf180
 test/test_helper.sh riscv32i gf180 &
 test/test_helper.sh uart-blocks gf180 &
@@ -25,6 +37,7 @@ wait
 test/test_helper.sh gcd ihp-sg13g2 &
 test/test_helper.sh aes ihp-sg13g2 &
 test/test_helper.sh ibex ihp-sg13g2 &
+wait
 test/test_helper.sh jpeg ihp-sg13g2 &
 test/test_helper.sh riscv32i ihp-sg13g2 &
 test/test_helper.sh spi ihp-sg13g2 &
@@ -34,6 +47,7 @@ test/test_helper.sh gcd asap7 &
 test/test_helper.sh aes asap7 &
 test/test_helper.sh ethmac asap7 &
 test/test_helper.sh ibex asap7 &
+wait
 test/test_helper.sh jpeg asap7 &
 test/test_helper.sh uart asap7 &
 test/test_helper.sh riscv32i asap7 &
