3ac15bb3fc2cbe83f577a3c2c89ee57a18d581ac
Augusto Berndt <augusto.berndt@precisioninno.com>
2024-11-29 14:48:38 +0000

    fix get_rudy and Makefile after merge with master
    Signed-off-by: Augusto Berndt <augusto.berndt@precisioninno.com>

--- flow/scripts/get_rudy.tcl
+++ flow/scripts/get_rudy.tcl
@@ -1,19 +1,14 @@
-source scripts/my_gui.tcl
-# $design_stage defined at gui.tcl
+source scripts/open.tcl
 
-gui::load_drc $::env(REPORTS_DIR)/5_route_drc.rpt-5.rpt
-set psName $::env(REPORTS_DIR)/$::env(DESIGN_NAME)-$::env(PLATFORM)-stg$design_stage-DRC.png
-save_image -width 3000 $psName
-
-gui::dump_heatmap Routing "$::env(REPORTS_DIR)/$::env(DESIGN_NAME)-$::env(PLATFORM)-stg$design_stage-grt.csv"
-gui::set_display_controls "Heat Maps/Routing Congestion" visible true
-set psName $::env(REPORTS_DIR)/$::env(DESIGN_NAME)-$::env(PLATFORM)-stg$design_stage-grt.png
-save_image -width 3000 $psName
-
-gui::dump_heatmap RUDY    "$::env(REPORTS_DIR)/$::env(DESIGN_NAME)-$::env(PLATFORM)-stg$design_stage-rudy.csv"
+gui::dump_heatmap RUDY "$::env(REPORTS_DIR)/$::env(PLATFORM)-$::env(my_design_name)-stg$::env(STAGE)-rudy.csv"
 gui::set_display_controls "Heat Maps/Estimated Congestion (RUDY)" visible true
-set psName $::env(REPORTS_DIR)/$::env(DESIGN_NAME)-$::env(PLATFORM)-stg$design_stage-rudy.png
+set psName $::env(REPORTS_DIR)/$::env(PLATFORM)-$::env(my_design_name)-stg$::env(STAGE)-rudy.png
 save_image -width 3000 $psName
 
-unset sdc_file s design_stage
+# gui::dump_heatmap Routing "$::env(REPORTS_DIR)/$::env(PLATFORM)-$::env(my_design_name)-stg$::env(STAGE)-grt.csv"
+# gui::set_display_controls "Heat Maps/Routing Congestion" visible true
+# set psName $::env(REPORTS_DIR)/$::env(PLATFORM)-$::env(my_design_name)-stg$::env(STAGE)-grt.png
+# save_image -width 3000 $psName
+
 exit
+
